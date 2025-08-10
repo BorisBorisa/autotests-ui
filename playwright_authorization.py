b3fc2a8a3c5bc284e8b4db4e1670d80dc7d78390
Aleksandr Borisov <BorisaGH@yandex.ru>
2025-08-10 05:33:13 +0300

    Введение в Playwright

--- playwright_authorization.py
+++ playwright_authorization.py
@@ -5,18 +5,17 @@ with sync_playwright() as playwright:
     page = browser.new_page()
     page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
 
-    email_locator = page.locator('//*[@data-testid="login-form-email-input"]//input')
+    email_locator = page.get_by_test_id("login-form-email-input").locator("input")
     email_locator.fill("user.name@gmail.com")
 
-    password_input = page.locator('//*[@data-testid="login-form-password-input"]//input')
+    password_input = page.get_by_test_id("login-form-password-input").locator("input")
     password_input.fill("Password")
 
-    login_button = page.locator('//button[@data-testid="login-page-login-button"]')
+    login_button = page.get_by_test_id("login-page-login-button")
     login_button.click()
 
-    wrong_email_or_password_alert = page.locator('//*[@data-testid="login-page-wrong-email-or-password-alert"]')
+    wrong_email_or_password_alert = page.get_by_test_id("login-page-wrong-email-or-password-alert")
     expect(wrong_email_or_password_alert).to_be_visible()
     expect(wrong_email_or_password_alert).to_have_text("Wrong email or password")
 
-
-    page.wait_for_timeout(2000)
+    page.wait_for_timeout(500)
