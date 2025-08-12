71a00f7929028b1e065db353f23705315f55479e
Aleksandr Borisov <BorisaGH@yandex.ru>
2025-08-12 17:56:20 +0300

    Работа с авторизацией в Playwright

--- playwright_registration.py
+++ playwright_registration.py
@@ -1,8 +1,14 @@
 from playwright.sync_api import sync_playwright, expect
 
+EMAIL = "user.name@gmail.com"
+USERNAME = "username"
+PASSWORD = "password"
+
 with sync_playwright() as playwright:
     browser = playwright.chromium.launch(headless=False)
-    page = browser.new_page()
+    context = browser.new_context()
+    page = context.new_page()
+
     page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
 
     email_input = page.get_by_test_id("registration-form-email-input").locator("input")
@@ -10,9 +16,9 @@ with sync_playwright() as playwright:
     password_input = page.get_by_test_id("registration-form-password-input").locator("input")
     registration_button = page.get_by_test_id("registration-page-registration-button")
 
-    email_input.fill("user.name@gmail.com")
-    username_input.fill("username")
-    password_input.fill("password")
+    email_input.fill(EMAIL)
+    username_input.fill(USERNAME)
+    password_input.fill(PASSWORD)
     registration_button.click()
 
     dashboard_title = page.get_by_test_id("dashboard-toolbar-title-text")
@@ -20,4 +26,13 @@ with sync_playwright() as playwright:
     expect(dashboard_title).to_be_visible()
     expect(dashboard_title).to_have_text("Dashboard")
 
-    page.wait_for_timeout(1000)
+    context.storage_state(path="browser-state.json")
+
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context(storage_state="browser-state.json")
+    page = context.new_page()
+
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard")
+
+    page.wait_for_timeout(2000)
