8ae68d4638ceaeefeaf417025ad38e7e9b6194bd
Aleksandr Borisov <BorisaGH@yandex.ru>
2025-08-15 10:44:18 +0300

    fix: сделал локаторы публичными и исправил порядок инициализации
    - self.__locator → self.locator
    - super().__init__() теперь вызывается до инициализации локаторов

--- pages/dashboard_page.py
+++ pages/dashboard_page.py
@@ -6,10 +6,10 @@ class DashboardPage(BasePage):
     DASHBOARD_TITLE_TEXT = "Dashboard"
 
     def __init__(self, page: Page):
-        self.__dashboard_title = page.get_by_test_id("dashboard-toolbar-title-text")
-
         super().__init__(page)
 
+        self.dashboard_title = page.get_by_test_id("dashboard-toolbar-title-text")
+
     def check_visible_dashboard_title(self):
-        expect(self.__dashboard_title).to_be_visible()
-        expect(self.__dashboard_title).to_have_text(self.DASHBOARD_TITLE_TEXT)
+        expect(self.dashboard_title).to_be_visible()
+        expect(self.dashboard_title).to_have_text(self.DASHBOARD_TITLE_TEXT)
--- pages/login_page.py
+++ pages/login_page.py
@@ -6,27 +6,27 @@ class LoginPage(BasePage):
     WRONG_EMAIL_OR_PASS_ALERT_TEXT = "Wrong email or password"
 
     def __init__(self, page: Page):
-        self.__email_input = page.get_by_test_id("login-form-email-input").locator("input")
-        self.__password_input = page.get_by_test_id("login-form-password-input").locator("input")
-        self.__logint_btn = page.get_by_test_id("login-page-login-button")
-        self.__registration_link = page.get_by_test_id("login-page-registration-link")
-        self.__wrong_email_or_password_alert = page.get_by_test_id("login-page-wrong-email-or-password-alert")
-
         super().__init__(page)
 
-    def fill_login_form(self, email: str, password: str) -> None:
-        self.__email_input.fill(email)
-        expect(self.__email_input).to_have_value(email)
+        self.email_input = page.get_by_test_id("login-form-email-input").locator("input")
+        self.password_input = page.get_by_test_id("login-form-password-input").locator("input")
+        self.logint_btn = page.get_by_test_id("login-page-login-button")
+        self.registration_link = page.get_by_test_id("login-page-registration-link")
+        self.wrong_email_or_password_alert = page.get_by_test_id("login-page-wrong-email-or-password-alert")
 
-        self.__password_input.fill(password)
-        expect(self.__password_input).to_have_value(password)
+    def fill_login_form(self, email: str, password: str) -> None:
+        self.email_input.fill(email)
+        expect(self.email_input).to_have_value(email)
+
+        self.password_input.fill(password)
+        expect(self.password_input).to_have_value(password)
 
     def click_login_button(self) -> None:
-        self.__logint_btn.click()
+        self.logint_btn.click()
 
     def click_registration_link(self) -> None:
-        self.__registration_link.click()
+        self.registration_link.click()
 
     def check_visible_wrong_email_or_password_alert(self) -> None:
-        expect(self.__wrong_email_or_password_alert).to_be_visible()
-        expect(self.__wrong_email_or_password_alert).to_have_text(self.WRONG_EMAIL_OR_PASS_ALERT_TEXT)
+        expect(self.wrong_email_or_password_alert).to_be_visible()
+        expect(self.wrong_email_or_password_alert).to_have_text(self.WRONG_EMAIL_OR_PASS_ALERT_TEXT)
