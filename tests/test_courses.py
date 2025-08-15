c761613780a643c1c0684631a07cfe1c001d1678
Aleksandr Borisov <BorisaGH@yandex.ru>
2025-08-15 17:06:16 +0300

    Практикуемся в реализации PageObject, Практикуемся в написании автотестов с использованием POM

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,11 +1,29 @@
 import pytest
 
 from playwright.sync_api import expect, Page
+from pages.courses_list_page import CoursesListPage
+from pages.create_course_page import CreateCoursePage
 
 COURSE_TITLE = "Courses"
 COURSE_LIST_TITLE = "There is no results"
 COURSE_LIST_DESCRIPTION_TEXT = "Results from the load test pipeline will be displayed here"
 COURSE_URL = "https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses"
+
+CREATE_COURSE_URL = "https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create"
+CREATE_COURSE_DEFAULT_DATA = {
+    "title": "",
+    "estimated_time": "",
+    "description": "",
+    "max_score": "0",
+    "min_score": "0"
+}
+CREATE_COURSE_TEST_DATA = {
+    "title": "Playwright",
+    "estimated_time": "2 weeks",
+    "description": "Playwright",
+    "max_score": "100",
+    "min_score": "10"
+}
 
 
 @pytest.mark.courses
@@ -28,3 +46,37 @@ def test_empty_courses_list(chromium_page_with_state: Page):
 
     expect(courses_list_description_text).to_be_visible()
     expect(courses_list_description_text).to_have_text(COURSE_LIST_DESCRIPTION_TEXT)
+
+
+@pytest.mark.courses
+@pytest.mark.regression
+def test_create_course(create_course_page: CreateCoursePage, courses_list_page: CoursesListPage):
+    create_course_page.visit(CREATE_COURSE_URL)
+
+    create_course_page.check_visible_create_course_title()
+    create_course_page.check_disabled_create_course_button()
+    create_course_page.check_visible_image_preview_empty_view()
+    create_course_page.check_visible_image_upload_view()
+
+    create_course_page.check_visible_create_course_form(**CREATE_COURSE_DEFAULT_DATA)
+
+    create_course_page.check_visible_exercises_title()
+    create_course_page.check_visible_create_exercise_button()
+    create_course_page.check_visible_exercises_empty_view()
+
+    create_course_page.upload_preview_image("./testdata/files/image.png")
+    create_course_page.check_visible_image_upload_view()
+
+    create_course_page.fill_create_course_form(**CREATE_COURSE_TEST_DATA)
+
+    create_course_page.click_create_course_button()
+
+    courses_list_page.check_visible_courses_title()
+    courses_list_page.check_visible_create_course_button()
+    courses_list_page.check_visible_course_card(
+        index=0,
+        title=CREATE_COURSE_TEST_DATA["title"],
+        max_score=CREATE_COURSE_TEST_DATA["max_score"],
+        min_score=CREATE_COURSE_TEST_DATA["min_score"],
+        estimated_time=CREATE_COURSE_TEST_DATA["estimated_time"]
+    )
