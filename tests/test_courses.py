c271e571615fc24b07575dcb62c030059bc81211
Aleksandr Borisov <BorisaGH@yandex.ru>
2025-08-16 20:26:40 +0300

    Практикуемся в реализации PageComponent

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -73,7 +73,7 @@ def test_create_course(create_course_page: CreateCoursePage, courses_list_page: 
 
     courses_list_page.check_visible_courses_title()
     courses_list_page.check_visible_create_course_button()
-    courses_list_page.check_visible_course_card(
+    courses_list_page.course_view.check_visible(
         index=0,
         title=CREATE_COURSE_TEST_DATA["title"],
         max_score=CREATE_COURSE_TEST_DATA["max_score"],
