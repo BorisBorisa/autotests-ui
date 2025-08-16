48bfd7c3ad00470999263e74add8d9c15ab31dd3
Aleksandr Borisov <BorisaGH@yandex.ru>
2025-08-17 01:01:21 +0300

    Практикуемся в реализации PageComponent

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -42,8 +42,8 @@ def test_create_course(create_course_page: CreateCoursePage, courses_list_page: 
 
     create_course_page.check_visible_create_course_title()
     create_course_page.check_disabled_create_course_button()
-    create_course_page.check_visible_image_preview_empty_view()
-    create_course_page.check_visible_image_upload_view()
+
+    create_course_page.image_upload_widget.check_visible(is_image_uploaded=False)
 
     create_course_page.check_visible_create_course_form(**CREATE_COURSE_DEFAULT_DATA)
 
@@ -51,8 +51,8 @@ def test_create_course(create_course_page: CreateCoursePage, courses_list_page: 
     create_course_page.check_visible_create_exercise_button()
     create_course_page.check_visible_exercises_empty_view()
 
-    create_course_page.upload_preview_image("./testdata/files/image.png")
-    create_course_page.check_visible_image_upload_view()
+    create_course_page.image_upload_widget.upload_preview_image("./testdata/files/image.png")
+    create_course_page.image_upload_widget.check_visible(is_image_uploaded=True)
 
     create_course_page.fill_create_course_form(**CREATE_COURSE_TEST_DATA)
 
