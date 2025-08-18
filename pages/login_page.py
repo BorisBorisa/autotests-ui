c932e474135b768f1181f3769ffaa4386417f2ec
Aleksandr Borisov <BorisaGH@yandex.ru>
2025-08-18 13:30:28 +0300

    Знакомство с PageFactory

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,3 +1,6 @@
+from elements.button import Button
+from elements.link import Link
+from elements.text import Text
 from pages.base_page import BasePage
 from components.authentication.login_form_component import LoginFormComponent
 
@@ -12,16 +15,18 @@ class LoginPage(BasePage):
 
         self.login_form = LoginFormComponent(page)
 
-        self.logint_button = page.get_by_test_id("login-page-login-button")
-        self.registration_link = page.get_by_test_id("login-page-registration-link")
-        self.wrong_email_or_password_alert = page.get_by_test_id("login-page-wrong-email-or-password-alert")
+        self.login_button = Button(page, "login-page-login-button", "Login")
+        self.registration_link = Link(page, "login-page-registration-link", "Link")
+        self.wrong_email_or_password_alert = Text(
+            page, "login-page-wrong-email-or-password-alert", "Wrong email or password"
+        )
 
     def click_login_button(self) -> None:
-        self.logint_button.click()
+        self.login_button.click()
 
     def click_registration_link(self) -> None:
         self.registration_link.click()
 
     def check_visible_wrong_email_or_password_alert(self) -> None:
-        expect(self.wrong_email_or_password_alert).to_be_visible()
-        expect(self.wrong_email_or_password_alert).to_have_text(self.WRONG_EMAIL_OR_PASS_ALERT_TEXT)
+        self.wrong_email_or_password_alert.check_visible()
+        self.wrong_email_or_password_alert.check_have_text(self.WRONG_EMAIL_OR_PASS_ALERT_TEXT)
