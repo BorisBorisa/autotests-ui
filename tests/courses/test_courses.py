bb9edebbeb6da4635a427728aa9977136a437bf6
Aleksandr Borisov <BorisaGH@yandex.ru>
2025-08-19 15:42:59 +0300

    Наращивание тестовой базы и рефакторинг. Практикуемся в написании автотестов с использованием Page Object, Page Component, Page Factory

--- tests/courses/test_courses.py
+++ tests/courses/test_courses.py
@@ -24,6 +24,13 @@ class TestCourses:
         "max_score": "100",
         "min_score": "10"
     }
+    UPDATE_COURSE_TEST_DATA = {
+        "title": "Selenium",
+        "estimated_time": "1 weeks",
+        "description": "Selenium",
+        "max_score": "5",
+        "min_score": "1"
+    }
 
     def test_empty_courses_list(self, courses_list_page: CoursesListPage):
         courses_list_page.visit(self.COURSE_URL)
@@ -59,4 +66,32 @@ class TestCourses:
             max_score=self.CREATE_COURSE_TEST_DATA["max_score"],
             min_score=self.CREATE_COURSE_TEST_DATA["min_score"],
             estimated_time=self.CREATE_COURSE_TEST_DATA["estimated_time"]
+        )
+
+    def test_edit_course(self, create_course_page: CreateCoursePage, courses_list_page: CoursesListPage):
+        create_course_page.visit(self.CREATE_COURSE_URL)
+
+        create_course_page.create_course_from.fill(**self.CREATE_COURSE_TEST_DATA)
+        create_course_page.image_upload_widget.upload_preview_image("./testdata/files/image.png")
+        create_course_page.create_course_toolbar_view.click_create_course_button()
+
+        courses_list_page.course_view.check_visible(
+            index=0,
+            title=self.CREATE_COURSE_TEST_DATA["title"],
+            max_score=self.CREATE_COURSE_TEST_DATA["max_score"],
+            min_score=self.CREATE_COURSE_TEST_DATA["min_score"],
+            estimated_time=self.CREATE_COURSE_TEST_DATA["estimated_time"]
+        )
+
+        courses_list_page.course_view.menu.click_edit(index=0)
+
+        create_course_page.create_course_from.fill(**self.UPDATE_COURSE_TEST_DATA)
+        create_course_page.create_course_toolbar_view.click_create_course_button()
+
+        courses_list_page.course_view.check_visible(
+            index=0,
+            title=self.UPDATE_COURSE_TEST_DATA["title"],
+            max_score=self.UPDATE_COURSE_TEST_DATA["max_score"],
+            min_score=self.UPDATE_COURSE_TEST_DATA["min_score"],
+            estimated_time=self.UPDATE_COURSE_TEST_DATA["estimated_time"]
         )
